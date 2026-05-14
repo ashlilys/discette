3b231cdb1ddf4647c8c200becb881b28945afda7
ashlilys <ashlii.drawing@gmail.com>
2026-05-14 04:08:20 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ let cds = [];
 let hoveredCD = null;
 let hasClicked = false;
 
+// your design/background image size
+let designW = 1366;
+let designH = 768;
+
 function preload() {
   bg = loadImage("main_bg.png");
   cdPlayer = loadImage("cd_player.png");
@@ -54,12 +58,12 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1920, 1080);
+  createCanvas(windowWidth, windowHeight);
   textAlign(CENTER, CENTER);
 
   for (let cd of cds) {
-    cd.w = 340;
-    cd.h = 130;
+    cd.w = 260;
+    cd.h = 95;
   }
 }
 
@@ -69,8 +73,8 @@ function draw() {
   imageMode(CORNER);
   image(bg, 0, 0, width, height);
 
-  // CD player at top-left coordinate
-  image(cdPlayer, 629, 115, 360, 360);
+  // CD player position: based on your design coordinates
+  image(cdPlayer, sx(629), sy(115), sw(300), sh(300));
 
   // check hover from top to bottom
   for (let i = cds.length - 1; i >= 0; i--) {
@@ -80,29 +84,38 @@ function draw() {
     }
   }
 
-  // draw bottom to top
+  // draw from bottom to top
   for (let i = 0; i < cds.length; i++) {
     let cd = cds[i];
-    let s = hoveredCD === cd ? 1.08 : 1;
+    let popping = hoveredCD === cd;
+    let popScale = popping ? 1.08 : 1;
+
+    let drawX = sx(cd.x);
+    let drawY = sy(cd.y);
+    let drawW = sw(cd.w);
+    let drawH = sh(cd.h);
 
     push();
-    translate(cd.x + cd.w / 2, cd.y + cd.h / 2);
-    scale(s);
     imageMode(CENTER);
-    image(cd.stackImg, 0, 0, cd.w, cd.h);
+    translate(drawX + drawW / 2, drawY + drawH / 2);
+    scale(popScale);
+    image(cd.stackImg, 0, 0, drawW, drawH);
     pop();
   }
 
-  // hover preview with original cd_# color
+  // hover preview cd_#
   if (hoveredCD) {
     imageMode(CORNER);
-    image(hoveredCD.previewImg, 558, 253, 230, 230);
 
+    // preview CD position
+    image(hoveredCD.previewImg, sx(558), sy(253), sw(220), sh(220));
+
+    // one text label only
     fill(255);
     stroke(0);
     strokeWeight(5);
-    textSize(34);
-    text(hoveredCD.label, 673, 220);
+    textSize(sw(30));
+    text(hoveredCD.label, sx(668), sy(220));
   }
 }
 
@@ -120,9 +133,30 @@ function mousePressed() {
 
 function isInsideCD(cd) {
   return (
-    mouseX > cd.x &&
-    mouseX < cd.x + cd.w &&
-    mouseY > cd.y &&
-    mouseY < cd.y + cd.h
+    mouseX > sx(cd.x) &&
+    mouseX < sx(cd.x) + sw(cd.w) &&
+    mouseY > sy(cd.y) &&
+    mouseY < sy(cd.y) + sh(cd.h)
   );
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+// scale helpers
+function sx(x) {
+  return x * (width / designW);
+}
+
+function sy(y) {
+  return y * (height / designH);
+}
+
+function sw(w) {
+  return w * (width / designW);
+}
+
+function sh(h) {
+  return h * (height / designH);
 }
