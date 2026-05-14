f4eb93077160bdc20af7b0e057ac12193681f873
ashlilys <ashlii.drawing@gmail.com>
2026-05-14 02:58:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,69 +1,113 @@
 let bg;
+let cdPlayer;
 let cds = [];
+let hoveredCD = null;
 let hasClicked = false;
 
 function preload() {
   bg = loadImage("main_bg.png");
+  cdPlayer = loadImage("cd_player.png");
 
+  // order is TOP to BOTTOM
   cds = [
-    { img: loadImage("cd_stack_1.png"), link: "deep_diving.html", label: "Deep Diving" },
-    { img: loadImage("cd_stack_2.png"), link: "barbie_girl.html", label: "Barbie Girl" },
-    { img: loadImage("cd_stack_3.png"), link: "natsuyuusora.html", label: "Natsu Yuuzora" },
-    { img: loadImage("cd_stack_4.png"), link: "guess.html", label: "Guess" },
-    { img: loadImage("cd_stack_5.png"), link: "enemy.html", label: "Enemy" }
+    {
+      stackImg: loadImage("cd_stack_5.png"),
+      previewImg: loadImage("cd_5.png"),
+      link: "enemy.html",
+      label: "Enemy"
+    },
+    {
+      stackImg: loadImage("cd_stack_4.png"),
+      previewImg: loadImage("cd_4.png"),
+      link: "guess.html",
+      label: "Guess"
+    },
+    {
+      stackImg: loadImage("cd_stack_3.png"),
+      previewImg: loadImage("cd_3.png"),
+      link: "natsuyuusora.html",
+      label: "Natsu Yuuzora"
+    },
+    {
+      stackImg: loadImage("cd_stack_2.png"),
+      previewImg: loadImage("cd_2.png"),
+      link: "barbie_girl.html",
+      label: "Barbie Girl"
+    },
+    {
+      stackImg: loadImage("cd_stack_1.png"),
+      previewImg: loadImage("cd_1.png"),
+      link: "deep_diving.html",
+      label: "Deep Diving"
+    }
   ];
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
-  positionCDs();
+  textAlign(CENTER, CENTER);
+  positionObjects();
 }
 
 function draw() {
+  hoveredCD = null;
+
+  // background
   imageMode(CORNER);
   image(bg, 0, 0, width, height);
 
   imageMode(CENTER);
 
-  for (let i = 0; i < cds.length; i++) {
-    let cd = cds[i];
-    let hovering = isInsideHitbox(cd);
+  // draw cd player on main page
+  image(cdPlayer, playerX, playerY, 360, 360);
 
-    let s = hovering ? 1.1 : 1;
+  // check hover first
+  for (let i = 0; i < cds.length; i++) {
+    if (isInsideCD(cds[i])) {
+      hoveredCD = cds[i];
+      break;
+    }
+  }
+
+  // draw stack from bottom to top
+  for (let i = cds.length - 1; i >= 0; i--) {
+    let cd = cds[i];
+    let popping = hoveredCD === cd;
+    let s = popping ? 1.08 : 1;
 
     push();
     translate(cd.x, cd.y);
     scale(s);
-    image(cd.img, 0, 0, cd.displayW, cd.displayH);
+    image(cd.stackImg, 0, 0, cd.displayW, cd.displayH);
     pop();
+  }
 
-    if (hovering) {
-      fill(255);
-      stroke(0);
-      strokeWeight(4);
-      textAlign(CENTER, CENTER);
-      textSize(32);
-      text(cd.label, width / 2, 90);
-    }
+  // preview CD + text on hover
+  if (hoveredCD) {
+    image(hoveredCD.previewImg, playerX, playerY - 40, 230, 230);
+
+    fill(255);
+    stroke(0);
+    strokeWeight(5);
+    textSize(34);
+    text(hoveredCD.label, playerX, playerY - 220);
   }
 }
 
 function mousePressed() {
   if (hasClicked) return;
 
-  for (let i = cds.length - 1; i >= 0; i--) {
-    let cd = cds[i];
-
-    if (isInsideHitbox(cd)) {
+  for (let i = 0; i < cds.length; i++) {
+    if (isInsideCD(cds[i])) {
       hasClicked = true;
-      window.location.href = cd.link;
+      window.location.href = cds[i].link;
       return;
     }
   }
 }
 
-function isInsideHitbox(cd) {
+function isInsideCD(cd) {
   return (
     mouseX > cd.x - cd.hitW / 2 &&
     mouseX < cd.x + cd.hitW / 2 &&
@@ -72,20 +116,27 @@ function isInsideHitbox(cd) {
   );
 }
 
-function positionCDs() {
+function positionObjects() {
+  stackX = width * 0.28;
+  stackStartY = height * 0.36;
+
+  playerX = width * 0.68;
+  playerY = height * 0.55;
+
   for (let i = 0; i < cds.length; i++) {
-    cds[i].displayW = 360;
-    cds[i].displayH = 135;
+    cds[i].x = stackX;
+    cds[i].y = stackStartY + i * 48;
 
-    cds[i].hitW = 160;
-    cds[i].hitH = 110;
+    cds[i].displayW = 330;
+    cds[i].displayH = 120;
 
-    cds[i].x = width / 2;
-    cds[i].y = height / 2 - 120 + i * 45;
+    // smaller hitbox so transparent PNG space doesn't mess up clicks
+    cds[i].hitW = 280;
+    cds[i].hitH = 45;
   }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  positionCDs();
+  positionObjects();
 }
